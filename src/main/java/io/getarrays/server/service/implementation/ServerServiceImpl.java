b589dcc6a2ad5b166899c77150e886d25e300790
Ira Strubhar <taylor.strubhar@gmail.com>
2023-03-29 10:51:39 -0700

    get method in SrvrSrvImpl still needs completion

--- src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -7,14 +7,19 @@ import io.getarrays.server.service.ServerService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static io.getarrays.server.enumeration.Status.SERVER_DOWN;
 import static io.getarrays.server.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.*;
+import static jdk.internal.module.ModulePath.of;
 
 @RequiredArgsConstructor
 @Service
@@ -43,25 +48,31 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = { "server1.png, server2.png, server3.png, server4.png," };
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
